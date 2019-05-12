ce18e05b0887f6deb244b8dc32e0d6f675e4b367
perlancar (on pc-home) <perlancar@gmail.com>
2019-05-12 11:26:02 +0700

    Initial commit

---
+++ lib/Text/CSV_XS/TSV.pm
@@ -0,0 +1,57 @@
+package Text::CSV_XS::TSV;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use parent 'Text::CSV_XS';
+
+sub new {
+    my $class = shift;
+
+    $opts = $_[0] ? { %{$_[0]} } : {};
+    $opts->{sep_char}    = "\t"  unless exists $opts->{sep_char};
+    $opts->{quote_char}  = undef unless exists $opts->{quote_char};
+    $opts->{escape_char} = undef unless exists $opts->{escape_char};
+
+    $self->SUPER::new($opts);
+}
+
+1;
+#ABSTRACT: Set Text::CSV_XS default options to parse TSV
+
+=head1 SYNOPSIS
+
+ use Text::CSV_XS::XS;
+
+
+=head1 DESCRIPTION
+
+This class is a simple subclass of L<Text::CSV_XS> to set the default of these
+options (if unspecified), suitable for parsing TSV (tab-separated values) files:
+
+=over
+
+=item * sep_char
+
+To C<"\t">.
+
+=item * quote_char
+
+To C<undef>.
+
+=item * escape_char
+
+To C<undef>.
+
+=back
+
+
+=head1 SEE ALSO
+
+L<Text::CSV_XS>
+
+=cut
